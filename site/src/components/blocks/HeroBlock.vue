d795dab91c3d70f72f21b816e0e099ff04208067
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-07-27 13:28:47 +0300

    init

--- site/src/components/blocks/HeroBlock.vue
+++ site/src/components/blocks/HeroBlock.vue
@@ -2,16 +2,25 @@
 </script>
 
 <template>
+  <div class="hero">
   <div class="hero__background_image">
     <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&w=800&q=80" alt="Background Image" />
   </div>
   <div class="hero__content">
-    asd
+    <h1>Platform Engineering That Works at Scale</h1>
+    <p>
+      I build and operate cloud-native infrastructure, CI/CD pipelines, and tools solving real-world engineering challenges. 
+      Go, Python, Kubernetes, AWS.
+      Clear, robust solutions from 10+ years in production.
+    </p>
+  </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .hero {
+  height: calc(100vh);
+
   &__background_image {
     position: absolute;
     right: 0px;
@@ -33,6 +42,8 @@
 
   &__content {
     display: flex;
+    justify-content: center;
+    flex-direction: column;
     align-items: center;
     height: 100%;
     width: 55%;
