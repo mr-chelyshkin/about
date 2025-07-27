c75cc22dce5324d979c2a21e688ba0bf8025f26d
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-07-27 13:11:03 +0300

    init

--- site/src/components/blocks/HeroBlock.vue
+++ site/src/components/blocks/HeroBlock.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+</script>
+
 <template>
+  <div class="background_image">
+    <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&w=800&q=80" alt="Background Image" />
+  </div>
+  <div>
     asd
+  </div>
 </template>
+
+<style scoped lang="scss">
+
+.background_image {
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  width: 45%;
+  height: 100%;
+  clip-path: polygon(30% 0%, 100% 0%, 100% 100%, 0% 100%);
+  
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+
+</style>
