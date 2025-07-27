38249c1b8c9f20ffad2a858ab54a30217e8bd5d2
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-07-27 13:03:11 +0300

    init

---
+++ site/src/layouts/DefaultLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="layout">
+    <header class="layout__header">
+      <div class="container">
+        <nav class="layout__nav">
+          <router-link to="/">Главная</router-link>
+          <router-link to="/tst">Тест</router-link>
+        </nav>
+      </div>
+    </header>
+
+    <main class="layout__main">
+      <div class="container">
+        <slot />
+      </div>
+    </main>
+
+    <footer class="layout__footer">
+      <div class="container">
+        &copy; {{ new Date().getFullYear() }} — Все права защищены.
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@use '@/assets/styles/variables' as *;
+
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  &__header {
+    background: #000;
+    color: #fff;
+    padding: 1rem;
+
+    .layout__nav {
+      display: flex;
+      gap: 1rem;
+      a {
+        color: white;
+        text-decoration: none;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+
+  &__main {
+    flex: 1 1 0%;
+    position: relative;
+  }
+
+  &__footer {
+    background: #f2f2f2;
+    padding: 1rem;
+    text-align: center;
+  }
+}
+</style>
