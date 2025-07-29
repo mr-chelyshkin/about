45d36cfd21f7515427f8d556435122b576345954
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-07-29 10:34:29 +0300

    init

--- site/src/components/items/ItemPSR.vue
+++ site/src/components/items/ItemPSR.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import ItemLayout from '@/components/layouts/CardLayout.vue';
+import LayoutCard from '@/components/layouts/LayoutCard.vue';
 
 interface Props {
   problem: string;
@@ -12,7 +12,7 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <ItemLayout :href="props.href">
+  <LayoutCard :href="props.href">
     <div class="item__header">
       <div class="item__header-label h-label">Problem</div>
       <div class="item__header-text h-text">{{ problem }}</div>
@@ -27,5 +27,61 @@ const props = defineProps<Props>();
       <div class="item__footer-label f-label">Result</div>
       <div class="item__footer-text f-text">{{ result }}</div>
     </div>
-  </ItemLayout>
+  </LayoutCard>
 </template>
+
+<style scoped lang="scss">
+.item__header,
+.item__body,
+.item__footer {
+  margin-bottom: 1rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.item__header-label,
+.item__body-label,
+.item__footer-label {
+  font-family: $font-main;
+  font-size: $text-size-xsm;
+  text-transform: uppercase;
+  margin-bottom: 0.5rem;
+  letter-spacing: 1px;
+  font-weight: 700;
+}
+
+.item__header-text,
+.item__body-text,
+.item__footer-text {
+  font-size: $text-size-sm;
+  line-height: 1.4;
+}
+
+.h-label {
+  color: $color-text-accent;
+}
+
+.b-label {
+  color: $color-text-main;
+}
+
+.f-label {
+  color: $color-text-secondary;
+}
+
+.h-text {
+  color: $color-text-main;
+  font-weight: 600;
+}
+
+.b-text {
+  color: $color-text-main;
+}
+
+.f-text {
+  color: $color-text-secondary;
+  font-style: italic;
+}
+</style>
---
+++ site/src/components/layouts/LayoutCard.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+interface Props {
+  href?: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <a :href="href" class="card">
+    <slot/>
+  </a>
+</template>
+
+<style scoped lang="scss">
+.card {
+  background: $color-background-main;
+  border: 2px solid $color-text-main;
+
+  transition: all 0.3s ease;
+  text-decoration: none;
+  position: relative;
+  color: inherit;
+  display: block;
+  padding: 1.5rem;
+  height: 100%;
+  
+  &::before {
+    background: $color-text-main;
+    content: '';
+    transition: all 0.3s ease;
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    width: 20px;
+    height: 20px;
+  }
+  
+  &:hover {
+    border-color: $color-text-accent;
+
+    transform: translateY(-2px);
+    
+    &::before {
+      background: $color-text-accent;
+
+      width: 25px;
+      height: 25px;
+    }
+  }
+  
+  &::after {
+    display: none;
+  }
+}
+</style>
