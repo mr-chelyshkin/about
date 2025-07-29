c6845dc06b5ca5955ac0b82b130cad615f6aedbd
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-07-29 14:45:54 +0300

    init

--- site/src/data/hero/data.ts
+++ site/src/data/hero/data.ts
@@ -1,7 +1,7 @@
 export const heroData = {
   image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&w=800&q=80',
   name: 'Igor Chelyshkin',
-  title: 'Platform Engineer & Infrastructure Architect',
+  title: 'Sr Platform Engineer & Infrastructure Architect',
   description: `Hello, I'm a platform engineer with 10+ years of expertise delivering scalable, secure, and efficient cloud-native infrastructure. I turn complex DevOps challenges into clear, optimised and actionable solutions.`,
   tagline: `Let's build infrastructure you can trust.`,
   techStack: [
