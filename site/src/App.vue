040d4b7a9d76757e69c3a459c9ad8eabc2a37548
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-08-01 11:55:41 +0300

    [fix] meta-tags in index without js

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,22 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-
 import LayoutMain from '@/components/LayoutMain.vue'
-
-import { useSEO } from '@/composables/useSEO'
-import { defaultSEO } from '@/data/meta/tags'
-import { jsonLd } from '@/data/meta/jsonLd'
-
-const { updateMeta } = useSEO()
-
-onMounted(() => {
-  updateMeta(defaultSEO)
-
-  const script = document.createElement('script')
-  script.textContent = JSON.stringify(jsonLd)
-  script.type = 'application/ld+json'
-  document.head.appendChild(script)
-})
 </script>
 
 <template>
