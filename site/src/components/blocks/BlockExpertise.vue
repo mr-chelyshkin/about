d86e308819014d8e84b8be709fbcf0d624b973d1
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-11-21 15:04:05 +0400

    draft

--- site/src/components/blocks/BlockExpertise.vue
+++ site/src/components/blocks/BlockExpertise.vue
@@ -4,7 +4,6 @@ import ItemNoteText from '@/components/items/ItemNoteText.vue'
 import ItemListKV from '@/components/items/ItemListKV.vue'
 import BaseBlock from '@/components/base/BaseBlock.vue'
 
-import { philosophy } from '@/data/expertise/philosophy'
 import { skills } from '@/data/expertise/skills'
 import { career } from '@/data/expertise/career'
 
@@ -17,11 +16,6 @@ defineProps<{
 
 <template>
   <BaseBlock :title="title" :subtitle="subtitle" :number="blockNumber">
-    <ItemNoteText
-      :class="$style.noteMargin"
-      :header="philosophy.header"
-      :content="philosophy.content"
-    />
     <div :class="$style.blockExpertise">
       <div :class="$style.blockExpertiseLayout">
         <div :class="$style.blockExpertiseList">
