eee3948a7872a6ef13a5cbdee24c128e622eff55
mr-chelyshkin <argent-keel-0z@icloud.com>
2026-09-08 23:10:48 +0400

    fix ci/cd

--- tf/locals.tf
+++ tf/locals.tf
@@ -3,8 +3,8 @@ locals {
   cloudfront_origin_id = "s3-${var.site_bucket_name}"
 
   tags = {
-    ManagedBy   = "Terraform"
-    Project     = "mr-chelyshkin-site"
-    Repository  = "mr-chelyshkin/site"
+    ManagedBy  = "Terraform"
+    Project    = "mr-chelyshkin-site"
+    Repository = "mr-chelyshkin/site"
   }
 }
